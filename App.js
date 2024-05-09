d262c674d282208f7df593f55dbe5a962ea8abcf
Vanshika Raj <135754394+vanshi-2010@users.noreply.github.com>
2024-05-09 09:50:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+        <Layout></Layout>
+    </div>
+   
+  );
+}
+
+export default App;
